543f95f1628de6000ccfb3f8523692c9d09c94ff
vivianluomin <273104241@qq.com>
2018-08-18 21:41:48 +0800

    葬宝图，就是自长公共子序列

---
+++ src/ZCGGzixulie.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class ZCGGzixulie {
+
+    public static  void main(String[] args){
+
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        String t = in.nextLine();
+        int count = getS(s,t);
+        if(count == t.length()){
+            System.out.println("Yes");
+        }else {
+            System.out.println("No");
+        }
+
+    }
+
+    public static int getS(String s,String t){
+
+        int[][] value = new int[s.length()+1][t.length()+1];
+        for(int i = 0;i<s.length();i++){
+            value[i][0] = 0;
+        }
+
+        for (int j = 0;j<t.length();j++){
+            value[0][j] = 0;
+        }
+        for(int i =0;i<s.length();i++){
+            for(int j = 0;j<t.length();j++){
+                if(s.charAt(i) == t.charAt(j)){
+                    value[i+1][j+1] = value[i][j]+1;
+                }else {
+                    value[i+1][j+1] = Math.max(value[i+1][j],value[i][j+1]);
+                }
+            }
+        }
+
+        return value[s.length()][t.length()];
+    }
+
+    public static int getMax(int a,int b,int c){
+        int max = a;
+        if(max<b){
+            max = b;
+        }
+
+        if(max<c){
+            max = c;
+        }
+
+        return max;
+    }
+
+}
